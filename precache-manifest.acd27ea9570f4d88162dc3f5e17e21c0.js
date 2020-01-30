4451c1ee661162ac5f6d741cbdcb58e202deff33
Jethro Cheng <jethrocheng@gmail.com>
2020-01-30 12:28:28 -0500

    Updates

---
+++ precache-manifest.acd27ea9570f4d88162dc3f5e17e21c0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95a943194ea87bfa2fec1dcec2f96a60",
+    "url": "/worker-directory/index.html"
+  },
+  {
+    "revision": "22dcd0596ff61b550033",
+    "url": "/worker-directory/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b1c84205b3d11c88d282",
+    "url": "/worker-directory/static/css/main.d9b799a1.chunk.css"
+  },
+  {
+    "revision": "22dcd0596ff61b550033",
+    "url": "/worker-directory/static/js/2.29dae0ad.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/worker-directory/static/js/2.29dae0ad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b1c84205b3d11c88d282",
+    "url": "/worker-directory/static/js/main.53186c19.chunk.js"
+  },
+  {
+    "revision": "aa735d2d78413c7c060d",
+    "url": "/worker-directory/static/js/runtime-main.eafadbb8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/worker-directory/static/media/logo.5d5d9eef.svg"
+  }
+]);
